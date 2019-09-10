f82c7cd69de8ecd8c37726c7917619058d3ec884
isoflabon <ksyo1skyan@gmail.com>
2019-09-10 17:46:37 +0900

    rollback #login

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -108,7 +108,7 @@ class App < Sinatra::Base
 
   post '/login' do
     name = params[:name]
-    statement = db.prepare('SELECT id, name, password, salt FROM user WHERE name = ?')
+    statement = db.prepare('SELECT * FROM user WHERE name = ?')
     row = statement.execute(name).first
     if row.nil? || row['password'] != Digest::SHA1.hexdigest(row['salt'] + params[:password])
       return 403
